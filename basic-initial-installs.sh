15b81cefda84876380df53d372ba630f51cd2037
blueyes <brian.w.petroski@gmail.com>
2019-02-14 04:27:28 -0600

    Update basic-initial-installs.sh

--- basic-initial-installs.sh
+++ basic-initial-installs.sh
@@ -1,2 +1,34 @@
-# necessities to get shit going. random. update and upgrade beforehand
-sudo apt install git repo npm nodejs coreutils android-tools-adb android-tools-fastboot aptitude
+#!/bin/bash
+set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Brian Petroski
+# 
+##################################################################################################################
+#######software from 'normal' repositories
+sudo apt-get install -y catfish clementine curl dconf-cli dconf-editor dropbox evolution focuswriter geany geary gimp gpick
+sudo apt-get install -y glances gparted grsync hardinfo inkscape meld
+sudo apt-get install -y openshot pinta plank ppa-purge screenfetch  shutter nemo-compare 
+sudo apt-get install -y  vlc    
+sudo apt remove ssh 
+sudo apt install -y openssh-server openssh-client sshpass git repo npm nodejs coreutils  aptitude
+
+###############################################################################################
+
+# installation of zippers and unzippers
+sudo apt-get install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
+
+###############################################################################################
+
+
+#ending
+mkdir $HOME/Upload
+sudo apt-get -y update
+sudo apt-get -f -y install
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+
+echo "################################################################"
+echo "###################  software installed      ###################"
+echo "################################################################"

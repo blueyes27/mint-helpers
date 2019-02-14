15ab88e72c00dc5937fd3ecc4529c26a4742b110
blueyes <brian.w.petroski@gmail.com>
2019-02-14 04:52:20 -0600

    Update basic-initial-installs.sh

--- basic-initial-installs.sh
+++ basic-initial-installs.sh
@@ -12,6 +12,9 @@ sudo apt-get install -y openshot pinta plank ppa-purge screenfetch  shutter nemo
 sudo apt-get install -y  vlc    
 sudo apt remove ssh 
 sudo apt install -y openssh-server openssh-client sshpass git repo npm nodejs coreutils  aptitude
+sudo apt-get install -y build-essential qmlscene qt5-qmake qt5-default qtdeclarative5-dev
+sudo apt-get install -y qtdeclarative5-controls-plugin qtdeclarative5-qtquick2-plugin libqt5qml-graphicaleffects
+sudo apt-get install -y qtdeclarative5-dialogs-plugin qtdeclarative5-localstorage-plugin qtdeclarative5-window-plugin
 
 ###############################################################################################
 
@@ -32,3 +35,51 @@ sudo apt-get -y autoclean
 echo "################################################################"
 echo "###################  software installed      ###################"
 echo "################################################################"
+
+rm /tmp/google-chrome-stable_current_amd64.deb
+
+echo "downloading google chrome latest stable edition"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+rm /tmp/google-chrome-stable*.deb
+git config --global user.email "brian.w.petroski@gmail.com"
+git config --global user.name "blueyes27"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+
+### Delete old kernels dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
+
+##Boot repair 
+sudo add-apt-repository -y ppa:yannubuntu/boot-repair
+sudo apt update
+
+sudo add-apt-repository ppa:clipgrab-team/ppa -y
+sudo apt-get update
+sudo apt-get install clipgrab -y
+sudo apt install boot-repair
+
+sudo add-apt-repository ppa:numix/ppa -y
+
+
+# getting new info of this new repo
+sudo apt-get -y update
+
+# installing
+sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle
+
+wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.1/Stacer_1.0.1_amd64.deb -O /tmp/stacer.deb
+
+sudo dpkg -i /tmp/stacer.deb
+
+rm /tmp/stacer.deb
+
+sudo add-apt-repository ppa:gerardpuig/ppa -y
+sudo apt update
+sudo apt install -y ubuntu-cleaner
+
+sudo apt-add-repository -y ppa:teejee2008/ppa -y
+sudo apt-get update
+sudo apt-get install ukuu -y
+
+#######END
